86a5ecc07fa4668c8590e03215011ba8a9691bbe
João Victor Bispo e Almeida <joaovictorbalmeida@hotmail.com>
2016-10-24 09:57:28 -0200

    crud

---
+++ public/main.js
@@ -0,0 +1,38 @@
+var update = document.getElementById('update'),
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Nome'
+      'quote': 'Citação'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(data => {
+    console.log(data)
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'teste'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  }).
+  then(data => {
+    console.log(data)
+    window.location.reload()
+  })
+})
